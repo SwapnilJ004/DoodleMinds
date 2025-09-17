48f3d9ef3e84023da50c3d46316f65296e55dee5
SwapnilJ004 <joshiswapnil2004@gmail.com>
2025-09-17 12:40:48 +0530

    Logo sound effect added

--- DoodleMinds/app/(tabs)/index.tsx
+++ DoodleMinds/app/(tabs)/index.tsx
@@ -1,36 +1,35 @@
 import { Text, View, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
 import { useEffect } from 'react';
 import { useRouter } from 'expo-router';
 import * as Animatable from 'react-native-animatable';
+import { useAudioPlayer } from 'expo-audio';
 
+import audioSource from '../../assets/logo_sound_effect.mp3';
 
 export default function Index() {
-const navigation = useRouter();
+  const navigation = useRouter();
+  const player = useAudioPlayer(audioSource);
 
   useEffect(() => {
-    setTimeout(()=> {
+    player.seekTo(0);
+    player.play();
+    setTimeout(() => {
       navigation.navigate('/(tabs)/juniorPlayback');
-    }, 3000);
+    }, 4000);
   }, []);
 
   return (
-    // <View>
-      <View style={{ backgroundColor: "black", flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Animatable.Text style={{color: 'white', fontSize:36, fontWeight: '800'}} duration={2000} animation={'fadeIn'}>
-          Doodle
-        </Animatable.Text>
-        <Animatable.Text style={{color: 'pink', fontSize:36, fontWeight: '800'}} duration={2000} animation={'fadeInUp'}>
-          Minds
-        </Animatable.Text>
-      </View>
-      /* <View style={styles.container}>
-        <Text style={styles.text}>Home screen</Text>
-        <Link href='./juniorPlayback' style={styles.button}>
-          Go to Home screen
-        </Link>
-      </View> */
-    
+    <View style={{ backgroundColor: "black", flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Animatable.Text style={{ color: 'white', fontSize: 36, fontWeight: '800' }} duration={2000} animation={'fadeIn'}>
+        Doodle
+      </Animatable.Text>
+      <Animatable.Text style={{ color: '#ff4d6d', fontSize: 36, fontWeight: '800' }} duration={2000} animation={'fadeInUp'}>
+        Minds
+      </Animatable.Text>
+      <Animatable.Text style={{ color: 'white', fontSize: 10, fontWeight: '400'}} duration={500} delay={2500} animation={'lightSpeedIn'}>
+        The mindful sketch!
+      </Animatable.Text>
+    </View>
   );
 }
 
